2e95da87f96442d10ba71d4fc3a1685ce428b2c7
Christopher Aliu <71184952+al-chris@users.noreply.github.com>
2024-03-29 23:43:24 +0100

    Update backend.js

--- js/backend.js
+++ js/backend.js
@@ -1,25 +1,5 @@
 const baseUrl = 'https://api.trendit3.com/api/admin';
 
-
-// Function to set cookie with dynamic expiry time
-function setCookie(name, value, days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000)); // Calculate expiry time
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-}
-
-
-
-// Retrieve cookie
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
-
-
 // get access token
 const accessToken = getCookie('accessToken');
 
