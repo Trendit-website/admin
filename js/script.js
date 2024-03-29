35f9975fb8d5b5fbd6c9747fe4d41645492752e7
Christopher Aliu <71184952+al-chris@users.noreply.github.com>
2024-03-29 23:43:09 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,26 @@
 
 // const { getAllUsers, displayAllUsers } = require('./backend.js');
 // import { getAllUsers, displayAllUsers } from './backend.js';
+
+
+// Function to set cookie with dynamic expiry time
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000)); // Calculate expiry time
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+
+
+// Retrieve cookie
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+
     
 //34a54bd75ca4415993cf00f7de1873ff7acb54a8
 document.addEventListener("DOMContentLoaded", function() {
