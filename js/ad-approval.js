d20c66cdc2308993588271e60b39851e1ebf3bc1
Christopher Aliu <71184952+al-chris@users.noreply.github.com>
2024-04-01 19:22:11 +0100

    Update ad-approval.js

--- js/ad-approval.js
+++ js/ad-approval.js
@@ -17,87 +17,85 @@ document.addEventListener("DOMContentLoaded", function() {
     // Function to fetch and display user data
     var data = getAllAds();
     // Display all users and execute the callback function once done
-    displayAllAds(data);
-});
+    displayAllAds(data, function() {
+        // Callback function to execute after users have been displayed
+        const boxes = document.querySelectorAll(".box1");
+        const earnBoxes = document.querySelectorAll(".earn-container .box1");
+        const popup = document.querySelector(".popup");
+        const popup2 = document.querySelector(".popup2");
+        const overlay = document.querySelector(".overlay");
+        const overlay2 = document.querySelector(".overlay2");
+        const cancelBtn = document.querySelector(".cancel-btn");
+        const saveBtn = document.querySelectorAll(".save-btn");
+        const approveBox = document.querySelector(".approve-box");
+        const approveYesBtn = document.querySelector(".approve-box .approve-yes");
+        const approveCancelBtns = document.querySelectorAll(".approve-box .cancel-btn");
+        const earnInfo = document.querySelector(".earn-info");
+        const goBackBtn = document.querySelector(".back");
+        const inviteBtn = document.getElementById('inviteBtn');
+        const inviteCancel = document.querySelectorAll('.invite-cancel');
 
+        boxes.forEach(box => {
+            box.addEventListener("click", function() {
+                console.log("clicked");
+                popup.style.display = "block";
+                overlay.style.display = "block";
+            });
+        });
 
+        earnBoxes.forEach(earnBox => {
+            earnBox.addEventListener("click", function() {
+                showearnInfo();
+            });
+        });
 
-// Callback function to execute after users have been displayed
-const boxes = document.querySelectorAll(".box1");
-const earnBoxes = document.querySelectorAll(".earn-container .box1");
-const popup = document.querySelector(".popup");
-const popup2 = document.querySelector(".popup2");
-const overlay = document.querySelector(".overlay");
-const overlay2 = document.querySelector(".overlay2");
-const cancelBtn = document.querySelector(".cancel-btn");
-const saveBtn = document.querySelectorAll(".save-btn");
-const approveBox = document.querySelector(".approve-box");
-const approveYesBtn = document.querySelector(".approve-box .approve-yes");
-const approveCancelBtns = document.querySelectorAll(".approve-box .cancel-btn");
-const earnInfo = document.querySelector(".earn-info");
-const goBackBtn = document.querySelector(".back");
-const inviteBtn = document.getElementById('inviteBtn');
-const inviteCancel = document.querySelectorAll('.invite-cancel');
+        cancelBtn.addEventListener("click", function() {
+            popup.style.display = "none";
+            overlay.style.display = "none";
+        });
 
-boxes.forEach(box => {
-    box.addEventListener("click", function() {
-        console.log("clicked");
-        popup.style.display = "block";
-        overlay.style.display = "block";
+        saveBtn.forEach(btn => {
+            btn.addEventListener("click", function() {
+                showApproveBox();
+            });
+        });
+
+        inviteBtn.addEventListener("click", function() {
+            showApproveBox();
+        });
+
+        goBackBtn.addEventListener("click", function() {
+            hideearnInfo();
+        });
+
+        approveYesBtn.addEventListener("click", function() {
+            hideApproveBox();
+            alert("Ad Approved!");
+        });
+
+        approveCancelBtns.forEach(cancel => {
+            cancel.addEventListener("click", function() {
+                hideApproveBox();
+            });
+        });
+
+        
+        // Event listeners for cancel buttons
+        document.getElementById("earn-appeal-cancel-btn").addEventListener("click", function() {
+            closeEarnAppealPopup();
+        });
+
+        document.getElementById("ad-approval-cancel-btn").addEventListener("click", function() {
+            closeAdApprovalPopup();
+        });
+
+        // Event listener for the "Invite" button to show the popup
+        inviteBtn.addEventListener('click', showPopup);
+
+        // Event listener for the cancel button to hide the popup
+        cancelButton.addEventListener('click', hidePopup);
     });
 });
-
-earnBoxes.forEach(earnBox => {
-    earnBox.addEventListener("click", function() {
-        showearnInfo();
-    });
-});
-
-cancelBtn.addEventListener("click", function() {
-    popup.style.display = "none";
-    overlay.style.display = "none";
-});
-
-saveBtn.forEach(btn => {
-    btn.addEventListener("click", function() {
-        showApproveBox();
-    });
-});
-
-inviteBtn.addEventListener("click", function() {
-    showApproveBox();
-});
-
-goBackBtn.addEventListener("click", function() {
-    hideearnInfo();
-});
-
-approveYesBtn.addEventListener("click", function() {
-    hideApproveBox();
-    alert("Ad Approved!");
-});
-
-approveCancelBtns.forEach(cancel => {
-    cancel.addEventListener("click", function() {
-        hideApproveBox();
-    });
-});
-
-
-// Event listeners for cancel buttons
-document.getElementById("earn-appeal-cancel-btn").addEventListener("click", function() {
-    closeEarnAppealPopup();
-});
-
-document.getElementById("ad-approval-cancel-btn").addEventListener("click", function() {
-    closeAdApprovalPopup();
-});
-
-// Event listener for the "Invite" button to show the popup
-inviteBtn.addEventListener('click', showPopup);
-
-// Event listener for the cancel button to hide the popup
-cancelButton.addEventListener('click', hidePopup);
 
 
   // Function to close Earn Appeal Popup
@@ -155,14 +153,15 @@ function hideApproveBox() {
 }
 
 
-const baseUrl = 'https://api.trendit3.com/api/admin';
 
-// get access token
-const accessToken = getCookie('accessToken');
 
 
 function getAllAds(page=1) {
 
+    const baseUrl = 'https://api.trendit3.com/api/admin';
+
+    // get access token
+    const accessToken = getCookie('accessToken');
   // Construct the full URL for the verification request
   const usersUrl = `${baseUrl}/tasks?page=${page}`;
   
