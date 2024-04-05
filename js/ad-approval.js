5e025c3d3b5df37f59876c09f2868034ecdb0ff9
JanetIfeoma <108590608+JanetIfeoma@users.noreply.github.com>
2024-04-05 13:29:18 +0100

    12

--- js/ad-approval.js
+++ js/ad-approval.js
@@ -81,7 +81,7 @@ function showTaskPopup(task) {
     overlay.style.display = "block";
 
     // Add click event listener to approve button in the popup
-    const approveButton = document.querySelector('.approve-btn');
+    const approveButton = document.querySelector('.save-btn');
     approveButton.addEventListener('click', function() {
         const taskId = task.id;
         approveTask(taskId)
