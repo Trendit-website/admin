af4239794fd51412eb18c1442e352bd9f62ef9e4
JanetIfeoma <108590608+JanetIfeoma@users.noreply.github.com>
2024-04-08 15:41:03 +0100

    13

--- js/ad-approval.js
+++ js/ad-approval.js
@@ -81,21 +81,25 @@ function showTaskPopup(task) {
 
     // Add click event listener to approve button in the popup
     const approveButton = document.querySelector('.save-btn');
-    approveButton.addEventListener('click', function() {
-        const taskId = task.id;
-        approveTask(taskId)
-            .then(response => {
-                console.log(response.message);
-                showApproveBox();
-                closeAdPopup();
-            })
-            .catch(error => console.error('Error approving task:', error));
+    approveButton.addEventListener('click', async function() {
+        try {
+            const taskId = task.id;
+            const response = await approveTask(taskId);
+            console.log(response.message);
+            showApproveBox();
+            closeAdPopup();
+        } catch (error) {
+            console.error('Error approving task:', error);
+        }
     });
+
+    // Add click event listener to close button in the popup
     const closeButton = document.querySelector('.cancel-btn');
     closeButton.addEventListener('click', function() {
         closeAdPopup();
     });
 }
+
 
 function closeAdPopup() {
     const popup = document.querySelector('.popup');
