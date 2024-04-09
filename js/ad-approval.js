5d5456360cc20d791f76a06b80ca5174dd437618
JanetIfeoma <108590608+JanetIfeoma@users.noreply.github.com>
2024-04-09 17:08:47 +0100

    14

--- js/ad-approval.js
+++ js/ad-approval.js
@@ -73,40 +73,66 @@ document.addEventListener("DOMContentLoaded", function() {
             })
             .catch(error => console.error('Error approving task:', error));
     });
-
-    // Add event listeners to top navigation links for filtering
-    const topNavLinks = document.querySelectorAll('.top-nav p');
-    topNavLinks.forEach(link => {
-        link.addEventListener('click', function() {
-            const status = link.textContent.toLowerCase();
-            filterAds(status);
-        });
-    });
-
-    // Add event listener to filter button
-    const filterButton = document.querySelector('.filter-btn');
-    filterButton.addEventListener('click', function() {
-        const filterOption = document.querySelector('.filter-option').value;
-        filterAdsByFilterOption(filterOption);
-    });
-
-    // Add event listener to sort button
-    const sortButton = document.querySelector('.sort-btn');
-    sortButton.addEventListener('click', function() {
-        const sortOption = document.querySelector('.sort-option').value;
-        sortAdsBySortOption(sortOption);
-    });
 });
 
-// Function to filter ads based on the selected status
-function filterAds(status) {
-    console.log("Filtering ads by:", status);
-    // Fetch ads based on status (You can modify this based on your API/data)
+function showTaskPopup(task) {
+    const popup = document.querySelector('.popup');
+    const overlay = document.querySelector('.overlay');
+
+    // Populate popup with task details
+    const taskDate = new Date(task.date_created).toLocaleString('en-US', { timeZone: 'GMT' });
+    const taskDescription = task.caption ? task.caption : `Like and follow ${task.platform} business pages`;
+    const taskEarning = `${task.total_allocated} per ${task.goal}`;
+
+    const popupContent = `
+        <button class="cancel-btn">&#10006;</button>
+        <div class="popup-box">
+            <div class="left">
+                <span>${taskDate}</span>
+                <p style="font-size: 20px;">${taskDescription}</p>
+                <div class="earning">
+                    <img src="./images/wallet.png" width="9px">
+                    <span>Earning:</span>
+                    <p>${taskEarning}</p>
+                </div>
+            </div>
+            <img src="./images/${task.platform}.png" width="300px">
+        </div>
+        <div class="info">
+            <!-- Populate with task details as needed -->
+        </div>
+        <!-- Save and Cancel buttons -->
+        <div class="checkout">
+            <span>Total Paid</span>
+            <p>${task.total_allocated}</p>
+            <button class="save-btn" data-task-id="${task.id}">Approve Ad &#x1F5F8;</button>
+        </div>
+    `;
+
+    popup.innerHTML = popupContent;
+    popup.style.display = "block";
+    overlay.style.display = "block";
+
+    // Add click event listener to close button in the popup
+    const closeButton = document.querySelector('.cancel-btn');
+    closeButton.addEventListener('click', function() {
+        closeAdPopup();
+    });
+}
+
+function closeAdPopup() {
+    const popup = document.querySelector('.popup');
+    const overlay = document.querySelector('.overlay');
+    popup.style.display = "none";
+    overlay.style.display = "none";
+}
+
+function getAllAds(page=1) {
     const baseUrl = 'https://api.trendit3.com/api/admin';
     const accessToken = getCookie('accessToken');
-    const tasksUrl = `${baseUrl}/tasks?status=${status}`;
-
-    fetch(tasksUrl, {
+    const tasksUrl = `${baseUrl}/tasks?page=${page}`;
+  
+    return fetch(tasksUrl, {
         method:'POST',
         headers: {
             'Authorization': `Bearer ${accessToken}`,
@@ -119,89 +145,147 @@ function filterAds(status) {
         }
         return response.json();
     })
-    .then(data => {
-        const adsContainer = document.getElementById('earn-container');
-        adsContainer.innerHTML = ''; // Clear existing ads
+    .catch((error) => {
+        console.error('Error', error);
+    });
+}
 
-        data.tasks.forEach(task => {
-            const adBox = createAdBox(task);
+async function displayAllAds(promise) {
+
+    try {
+        const response = await promise;
+        const data = response.tasks;
+
+        // Check if the data array exists and is not empty
+        if (!data || data.length === 0) {
+            console.log("No ads to display.");
+            return; // Exit the function if there are no ads
+        }
+
+        const adsContainer = document.getElementById('earn-container');
+
+        data.forEach(task => {
+            const adBox = document.createElement('div');
+            adBox.classList.add('box1');
+            adBox.setAttribute('data-task-id', task.id); // Set task ID as attribute
+
+            adBox.addEventListener("click", function() {
+                console.log("clicked");
+                showTaskPopup(task);
+            });
+
+            const statusDiv = document.createElement('div');
+            statusDiv.classList.add('pending');
+
+            const platformImage = document.createElement('img');
+            platformImage.src = `./images/${task.platform}.png`;
+            platformImage.alt = task.platform;
+
+            const statusParagraph = document.createElement('p');
+            statusParagraph.textContent = task.status.charAt(0).toUpperCase() + task.status.slice(1); // Capitalize first letter
+
+            statusDiv.appendChild(platformImage);
+            statusDiv.appendChild(statusParagraph);
+
+            const descriptionParagraph = document.createElement('p');
+            if (task.caption) {
+                descriptionParagraph.textContent = task.caption;
+            } else {
+                descriptionParagraph.textContent = `Like and follow ${task.platform} business pages`;
+            }
+
+            const dateSpan = document.createElement('span');
+            dateSpan.textContent = new Date(task.date_created).toLocaleString('en-US', { timeZone: 'GMT' }); // Convert to local time
+
+            const earningDiv = document.createElement('div');
+            earningDiv.classList.add('earning');
+
+            const earningImage = document.createElement('img');
+            earningImage.src = "./images/wallet.png";
+            earningImage.width = "9";
+
+            const earningSpan = document.createElement('span');
+            earningSpan.textContent = 'Earning:';
+
+            const earningParagraph = document.createElement('p');
+            earningParagraph.textContent = task.total_allocated + ' per ' + task.goal;
+
+            earningDiv.appendChild(earningImage);
+            earningDiv.appendChild(earningSpan);
+            earningDiv.appendChild(earningParagraph);
+
+            adBox.appendChild(statusDiv);
+            adBox.appendChild(descriptionParagraph);
+            adBox.appendChild(dateSpan);
+            adBox.appendChild(earningDiv);
+
             adsContainer.appendChild(adBox);
         });
+    } catch (error) {
+        console.error('Error displaying ads:', error);
+    }
+}
+
+function getTaskById(taskId) {
+    const baseUrl = 'https://api.trendit3.com/api/admin';
+    const accessToken = getCookie('accessToken');
+    const taskUrl = `${baseUrl}/tasks/${taskId}`;
+  
+    return fetch(taskUrl, {
+        method:'POST',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response=> {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
     })
     .catch((error) => {
-        console.error('Error filtering ads:', error);
+        console.error('Error', error);
     });
 }
 
-// Function to filter ads by specific filter option
-function filterAdsByFilterOption(filterOption) {
-    console.log("Filtering ads by filter option:", filterOption);
-    // Fetch ads based on filter option (You can modify this based on your API/data)
-}
-
-// Function to sort ads based on the selected sort option
-function sortAdsBySortOption(sortOption) {
-    console.log("Sorting ads by:", sortOption);
-    // Sort ads based on sort option (You can modify this based on your API/data)
-}
-
-// Function to create an ad box element
-function createAdBox(task) {
-    const adBox = document.createElement('div');
-    adBox.classList.add('box1');
-    adBox.setAttribute('data-task-id', task.id);
-
-    adBox.addEventListener("click", function() {
-        console.log("clicked");
-        showTaskPopup(task);
+function approveTask(taskId) {
+    const baseUrl = 'https://api.trendit3.com/api/admin';
+    const accessToken = getCookie('accessToken');
+    const approveUrl = `${baseUrl}/approve-task/${taskId}`;
+  
+    return fetch(approveUrl, {
+        method:'POST',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response=> {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .catch((error) => {
+        console.error('Error', error);
     });
+}
 
-    const statusDiv = document.createElement('div');
-    statusDiv.classList.add('pending');
+function showApproveBox() {
+    const approveBox = document.querySelector('.approve-box');
+    const overlay = document.querySelector('.overlay');
 
-    const platformImage = document.createElement('img');
-    platformImage.src = `./images/${task.platform}.png`;
-    platformImage.alt = task.platform;
+    approveBox.style.display = "block";
+    overlay.style.display = "block";
+}
 
-    const statusParagraph = document.createElement('p');
-    statusParagraph.textContent = task.status.charAt(0).toUpperCase() + task.status.slice(1);
+function closeApproveBox() {
+    const approveBox = document.querySelector('.approve-box');
+    const overlay = document.querySelector('.overlay');
 
-    statusDiv.appendChild(platformImage);
-    statusDiv.appendChild(statusParagraph);
-
-    const descriptionParagraph = document.createElement('p');
-    if (task.caption) {
-        descriptionParagraph.textContent = task.caption;
-    } else {
-        descriptionParagraph.textContent = `Like and follow ${task.platform} business pages`;
-    }
-
-    const dateSpan = document.createElement('span');
-    dateSpan.textContent = new Date(task.date_created).toLocaleString('en-US', { timeZone: 'GMT' });
-
-    const earningDiv = document.createElement('div');
-    earningDiv.classList.add('earning');
-
-    const earningImage = document.createElement('img');
-    earningImage.src = "./images/wallet.png";
-    earningImage.width = "9";
-
-    const earningSpan = document.createElement('span');
-    earningSpan.textContent = 'Earning:';
-
-    const earningParagraph = document.createElement('p');
-    earningParagraph.textContent = task.total_allocated + ' per ' + task.goal;
-
-    earningDiv.appendChild(earningImage);
-    earningDiv.appendChild(earningSpan);
-    earningDiv.appendChild(earningParagraph);
-
-    adBox.appendChild(statusDiv);
-    adBox.appendChild(descriptionParagraph);
-    adBox.appendChild(dateSpan);
-    adBox.appendChild(earningDiv);
-
-    return adBox;
+    approveBox.style.display = "none";
+    overlay.style.display = "none";
 }
 
 
