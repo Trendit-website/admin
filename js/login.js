bc457032a53de348b269043053e52c6d14c9e768
JanetIfeoma <108590608+JanetIfeoma@users.noreply.github.com>
2024-05-31 14:58:43 +0100

    42

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
       emailForm.addEventListener("submit", function(event) {
           event.preventDefault();
 
-          const baseUrl = 'https://api.trendit35.com/api/admin';
+          const baseUrl = 'https://api.trendit3.com/api/admin';
           const emailInput = document.getElementById("email");
           if (!emailInput) {
               console.error('Element with ID "email" not found');
