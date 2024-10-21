c28ec7f39963c6edacfde84520b27d42bb4fcf17
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2024-10-21 12:15:56 +0100

    feat: Implement user search function with email

--- src/api/useGetUsers.ts
+++ src/api/useGetUsers.ts
@@ -13,6 +13,22 @@ export const UseGetAllUsers = (page: number) => {
     isError: error,
   };
 };
+export const FilterUserEmail = (email: string) => {
+  const { data, error } = useSWR<UserSchema>(`/users?email=${email}`, ApiClient)
+  return {
+    user: data?.data,
+    isLoadingUser: !data,
+    isErrorUser: error
+  }
+}
+export const FilterUsername = (username: string) => {
+  const { data, error } = useSWR(`/users?username=${username}`, ApiClient)
+  return {
+    user: data,
+    isLoadingUser: !data,
+    isErrorUser: error
+  }
+}
 export const UseGetAllEarners = (page: number) => {
   const { data, error } = useSWR<UserSchema>(
     `/earners?page=${page}&per_page=15`,
--- src/components/users/UsersTable/earnersTable.tsx
+++ src/components/users/UsersTable/earnersTable.tsx
@@ -2,11 +2,26 @@ import Icons from "../../Shared/Icons";
 import Image from "next/image";
 import Link from "next/link";
 import { UseGetAllEarners } from "../../../api/useGetUsers";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import InputField from "../../Shared/InputField";
+import { FilterUserEmail } from "../../../api/useGetUsers";
 const EarnersTable = () => {
   const [activePage, setActivePage] = useState(1);
   const { allEarners, isLoading, isError } = UseGetAllEarners(activePage);
   const pages = Array.from({ length: allEarners?.pages ?? 1 }, (_, i) => i + 1);
+  const form = useForm()
+  const { watch, register } = form
+  const searchParam = watch("searchValue")
+  const [searchData, setSearchData] = useState<string | undefined>()
+  const { user, isLoadingUser, isErrorUser } = FilterUserEmail(searchData)
+  useEffect(() => {
+    if(searchParam) {
+      setSearchData(searchParam)
+    } else if(searchParam === '') {
+      setSearchData(undefined)
+    }
+  }, [searchParam, searchData])
   const NextPage = () => {
     if (allEarners?.pages) {
       activePage !== allEarners?.pages
@@ -52,10 +67,56 @@ const EarnersTable = () => {
                   Manage your team members and their account permissions here.
                 </span>
               </div>
+              <InputField register={register("searchValue")} type="search" placeholder="Enter User's Email" classNames="w-full rounded-[8px] py-[10px] pl-[10px] border-solid border-[#DOD5DD] border-[1px] focus:outline-main" containerStyle="w-[300px] ml-32"/>
               <div>
                 <Icons type="vertical-dot" />
               </div>
             </div>
+            {
+              searchParam && (
+                <div className="w-10/12 flex items-center justify-end -mt-2">
+                  <div className="bg-[#FFFFFF] flex flex-col rounded-[8px] border-[1px] border-borderColor border-solid h-[200px] w-[300px] z-10 -mb-48">
+                        {user?.users?.length > 0 && (
+                          user?.users?.map((user, index) => (
+                            <Link href={`/users/${user?.id}`} className="flex cursor-pointer items-center px-2 gap-x-4 w-full py-2 border-solid border-b-[1px] border-borderColor" key={index}>
+                               <Image
+                            src={user?.profile_picture || "/assets/Logo.svg"}
+                            width={40}
+                            height={40}
+                            className="rounded-[200px] w-[40px] h-[40px]"
+                            alt="avatar"
+                          />
+                          <div className="flex flex-col gap-y-[2px]">
+                            <p>{user?.full_name}</p>
+                            <span>{user?.email}   <b className="text-[10px]">({user?.username})</b></span>
+                          </div>
+                            </Link>
+                          ))
+                        )}
+                        {
+                          user?.users?.length <= 0 && (
+                            <div className="flex w-full text-red-500 justify-center py-2">
+                              User does not exist 
+                            </div>
+                          )
+                        }
+                        {
+                          isErrorUser && (
+                            <div className="flex w-full text-red-500 justify-center py-2">
+                                {isError?.response?.data?.message ||
+                                  " An error occured try again later"}
+                            </div>
+                          )
+                        }
+                         {isLoadingUser && !isError && (
+                          <div className="w-full py-8 flex justify-center">
+                            <Icons type="loader" />
+                          </div>
+                        )}
+                  </div>
+                </div>
+              )
+            }
             <table className="w-full flex flex-col">
               <thead className="w-full bg-[#F5F5F5] py-2 px-8 rounded-tr-[12px] rounded-tl-[12px]">
                 <tr className="flex items-center">
